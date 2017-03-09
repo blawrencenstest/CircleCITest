48dfebea7a918a302862032e2a4879828bda3e4b
Brian Lawrence <blawrence@nowsecure.com>
2017-03-09 14:50:46 -0600

    Circle setup

--- lab-auto.sh
+++ lab-auto.sh
@@ -1,9 +1,1 @@
-curl \
--X POST \
--H 'Authorization: Bearer '$API_TOKEN \
-https://lab-api.nowsecure.com/build/ --data-binary @$APP_FILE
-
-curl \
--X POST \
--H 'Authorization: Bearer '$API_TOKEN \
-https://lab-api.nowsecure.com/app/<platform>/<package>/assessment/
+echo "I AM SUCCEEDING"
