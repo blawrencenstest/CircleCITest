95ecce436b1af1af67c65d6177339f970def0102
Brian Lawrence <blawrence@nowsecure.com>
2017-03-10 11:59:13 -0600

    Never code test

--- lab-auto.sh
+++ lab-auto.sh
@@ -1,1 +1,11 @@
-echo "I AM SUCCEEDING"
+
+curl \
+-X POST \
+-H 'Authorization: Bearer '$LAB_KEY \
+https://lab-api.nowsecure.com/build/ --data-binary
+
+
+curl \
+-X POST \
+-H 'Authorization: Bearer '$LAB_KEY \
+https://lab-api.nowsecure.com/app/android/bscorp.appbase/assessment/
